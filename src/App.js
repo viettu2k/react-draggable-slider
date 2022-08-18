d9ced35f978b0c3f38f0f44905d7e0bc1ed6fc5a
viettu2k <doanviettu2k@gmail.com>
2022-08-18 09:04:21 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Slider from "react-draggable-slider";
+import { projectList } from "./dummy";
 
 function App() {
+  const sliderSettings = {
+    data: projectList,
+    speed: 3000,
+    bgColor: "#070707",
+    showButton: true,
+    buttonText: "View projects",
+    buttonHref: "https://www.example.com",
+    buttonTarget: "_self",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Slider sliderSettings={sliderSettings} />
     </div>
   );
 }
---
+++ src/dummy.js
@@ -0,0 +1,38 @@
+import project1 from "./images/bibi-pace-Hi4fWKU2KSk-unsplash.jpg";
+import project2 from "./images/scott-webb-e3dY8laAQtA-unsplash.jpg";
+import project3 from "./images/alexander-andrews-fsH1KjbdjE8-unsplash.jpg";
+import project4 from "./images/daan-mooij-RyxShs7mG8E-unsplash.jpg";
+import project5 from "./images/damien-santos-M5rim8Yh4LY-unsplash.jpg";
+
+export const projectList = [
+  {
+    title: "Nature's Life",
+    image: project1,
+    description:
+      "Praesent quis congue nisi. Vivamus dapibus suscipit magna at imperdiet. Mauris consectetur pharetra metus eu suscipit. Maecenas augue tortor, consequat vitae lacus id, pharetra tempor mauris. Suspendisse sodales commodo erat, non imperdiet nisl scelerisque at. Nulla porttitor gravida diam, in ornare ipsum accumsan bibendum. Morbi ut ante metus. ",
+  },
+  {
+    title: "Into The Waters",
+    image: project2,
+    description:
+      "Duis at tellus vitae velit aliquet varius. Fusce luctus libero et ligula tristique lobortis. Vestibulum eu placerat risus, eu semper augue. Integer at purus sit amet elit pretium viverra. Suspendisse id fringilla nibh, nec dictum urna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. ",
+  },
+  {
+    title: "Higher Than The Stars",
+    image: project3,
+    description:
+      "Praesent quis congue nisi. Vivamus dapibus suscipit magna at imperdiet. Maecenas augue tortor, consequat vitae lacus id, pharetra tempor mauris. Suspendisse sodales commodo erat, non imperdiet nisl scelerisque at. Nulla porttitor gravida diam, in ornare ipsum accumsan bibendum. Morbi ut ante metus. Proin rhoncus magna lectus, nec consequat augue ultricies eu.",
+  },
+  {
+    title: "Miniature Wonders",
+    image: project4,
+    description:
+      "Praesent quis congue nisi. Vivamus dapibus suscipit magna at imperdiet. Mauris consectetur pharetra metus eu suscipit. Maecenas augue tortor, consequat vitae lacus id, pharetra tempor mauris. Suspendisse sodales commodo erat, non imperdiet nisl scelerisque at. Nulla porttitor gravida diam, in ornare ipsum accumsan bibendum. Morbi ut ante metus. ",
+  },
+  {
+    title: "Giants Of History",
+    image: project5,
+    description:
+      "Duis at tellus vitae velit aliquet varius. Fusce luctus libero et ligula tristique lobortis. Vestibulum eu placerat risus, eu semper augue. Integer at purus sit amet elit pretium viverra. Suspendisse id fringilla nibh, nec dictum urna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. ",
+  },
+];
